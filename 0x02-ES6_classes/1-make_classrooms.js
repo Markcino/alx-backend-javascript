c22e73d24cbf1ae100892dda9a32351c0e6fd728
Mark Allen Gbalazeh <33788083+Markcino@users.noreply.github.com>
2024-06-07 09:38:18 +0000

    update

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,11 +1,9 @@
-import Classroom from './0-classroom';
+import ClassRoom from './0-classroom';
 
-function initializeRooms() {
-  return [
-    new Classroom(19),
-    new Classroom(20),
-    new Classroom(34),
-  ];
+export default function initializeRooms() {
+  // Call the map method on the array [19, 20, 34], which applies the
+  // provided function to each element in the array.
+  // The map method returns a new array containing the ClassRoom objects
+  // created by the provided function for each element in the original array.
+  return [19, 20, 34].map((size) => new ClassRoom(size));
 }
-
-export default initializeRooms;
